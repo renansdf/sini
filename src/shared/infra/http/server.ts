c40ab1ed0b80490d5dcb7eb4cff26a55878dc19b
Renan de Freitas <rnn.frts@gmail.com>
2020-06-28 19:03:24 -0300

    initial commit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
@@ -7,6 +8,7 @@ import '../typeorm';
 import '../containers';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
